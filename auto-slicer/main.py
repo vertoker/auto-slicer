ed0c89a0ae6fd8cb24c55e8a1de1376d031e4f3d
vertoker <vertogpro@gmail.com>
2022-09-25 15:13:54 +0500

    complete demo ui

--- auto-slicer/main.py
+++ auto-slicer/main.py
@@ -8,9 +8,12 @@ import staticdata
 import render
 
 data = [
-	[rnd.randint(1000000000, 9999999999), (1920, 1080), 'G:\\Projects\\Python\\AutoSlicer\\result.mp4'],
-	[],
-	[]
+	[rnd.randint(1000000000, 9999999999), (1920, 1080), 'G:\\Projects\\Python\\AutoSlicer\\test data\\result.mp4'],
+	[
+		['G:\\Projects\\Python\\AutoSlicer\\test data\\source1.mp4',
+		True, True, True, True, 3, 20, 0, 1963, 0]
+	],
+	['G:\\Projects\\Python\\AutoSlicer\\test data\\audio.mp3', 0, 0, 0, 0]
 ]
 
 updated = False
@@ -21,32 +24,26 @@ event, values = window.read()
 def logger(message):
 	window['logger'].update(value = message)
 
-def render():
-	logger('Загрузка субтитров')
-	#caption, name, language = sub.DownloadCaption(link)
-	logger('Форматирование текста')
-	#dataText = tf.TextFormater(caption, language)
-	logger('Расстановка пунктуации нейросетью')
-	#text = ai.PunctuationFormatter(dataText)
-	#global data, updated
-	#data = [text, name, caption]
-	logger('Текст готов')
+def renderVideo():
+	render.renderVideo(logger)
+	global updated
 	updated = False
-
-def selectPathSave():
-	open(gui2.filesavebox(default = data[1] + '.txt'), 'w').write(data[0])
-	logger('Текст сохранён')
 
 def updateAll():
 	languageUpdate()
 	seedUpdate()
 	resolutionUpdate()
+	videoPathUpdate()
+	audioPathUpdate()
+	videoResultPathUpdate()
 
 def languageUpdate():
 	global values
 	index = staticdata.languages.index(values['language_select'])
 	for key, value in staticdata.language_data.items():
 		window[key].update(value[index])
+	solution_combo = staticdata.video_solution[index]
+	window['video_solution_select'].update(values=solution_combo.Values)
 
 def seedUpdate():
 	global data
@@ -58,18 +55,48 @@ def resolutionUpdate():
 	window['video_resolution_heigth'].update(data[0][1][1])
 	window['video_resolution_select'].update(str(data[0][1][0]) + 'x' + str(data[0][1][1]))
 
+def videoPathOpen():
+	try:
+		open(gui2.fileopenbox(default = data[1][0][0] + '.mp4'), 'w').write(data[1][0][0])
+	except:
+		pass
+	videoPathUpdate()
+
+def audioPathOpen():
+	try:
+		open(gui2.fileopenbox(default = data[2][0] + '.mp3'), 'w').write(data[2][0])
+	except:
+		pass
+	audioPathUpdate()
+
+def videoResultPathOpen():
+	try:
+		open(gui2.filesavebox(default = data[0][2] + '.mp4'), 'w').write(data[0][2])
+	except:
+		pass
+	videoResultPathUpdate()
+
+def videoPathUpdate():
+	window['video_path_input'].update(data[1][0][0])
+
+def audioPathUpdate():
+	window['audio_path_input'].update(data[2][0])
+
+def videoResultPathUpdate():
+	window['result_video_path_input'].update(data[0][2])
+
 updateAll()
 
 
 while True:
 	event, values = window.read()
-	print(event, values) #debug
+	#print(event, values) #debug
 	if not updated:
-		if event in (None, 'Exit', 'Cancel'):
+		if event in (gui.WINDOW_CLOSED, "-ESCAPE-"):
 			break
 		if event == 'render':
 			updated = True
-			updater = th.Thread(target = render)
+			updater = th.Thread(target = renderVideo)
 			updater.start()
 		# Основное
 		if event == 'language_select':
@@ -91,19 +118,30 @@ while True:
 			data[0][1] = values['video_resolution_select'].split('x')
 			resolutionUpdate()
 		# Видео
-		if event == '':
-			pass
-		if event == '':
-			pass
-		if event == '':
-			pass
-		if event == '':
-			pass
-		if event == '':
-			pass
-		if event == '':
-			pass
-		if event == '':
-			pass
+		if event == 'video_path_button':
+			videoPathOpen()
+		if event == 'video_path_input':
+			data[1][0][0] = values['video_path_input']
+		if event == 'video_slice_checkbox':
+			data[1][0][1] = values['video_slice_checkbox']
+		if event == 'audio_mute_checkbox':
+			data[1][0][2] = values['audio_mute_checkbox']
+		if event == 'shuffle_separately_checkbox':
+			data[1][0][3] = values['shuffle_separately_checkbox']
+		if event == 'shuffle_in_general_checkbox':
+			data[1][0][4] = values['shuffle_in_general_checkbox']
+		if event == 'min_slice_length_input':
+			data[1][0][5] = values['min_slice_length_input']
+		if event == 'max_slice_length_input':
+			data[1][0][6] = values['max_slice_length_input']
+		if event == 'start_video_input':
+			data[1][0][7] = values['start_video_input']
+		if event == 'end_video_input':
+			data[1][0][8] = values['end_video_input']
+		# Аудио
+		if event == 'audio_path_button':
+			audioPathOpen()
+		if event == 'result_video_path_button':
+			videoResultPathOpen()
 		if event == '':
 			pass
--- auto-slicer/staticdata.py
+++ auto-slicer/staticdata.py
@@ -12,6 +12,21 @@ resolution_templates = [
     '1080x1080', '1920x1920'
 ]
 
+video_resolution_solutions_eng = [
+    'Paste video',
+    'Fill video',
+    'Stretch video'
+]
+video_resolution_solutions_rus = [
+    'Вставить видео',
+    'Заполнить видео',
+    'Растянуть видео'
+]
+video_solution = [
+    gui.Combo(video_resolution_solutions_eng, default_value=video_resolution_solutions_eng[0], key='video_solution_select', enable_events=True), 
+    gui.Combo(video_resolution_solutions_rus, default_value=video_resolution_solutions_rus[0], key='video_solution_select', enable_events=True)
+]
+
 language_data = {
     'language_text': ('Language', 'Язык'),
     'seed_refresh': ('Refresh', 'Обновить'),
@@ -19,18 +34,20 @@ language_data = {
     'video_resolution_text': ('Resolution', 'Разрешение'),
     'video_path_text': ('Video path', 'Путь к видео'),
     'video_path_button': ('Search', 'Найти'),
-    'video_slice_input': ('Slice video?', 'Нарезать видео?'),
-    'audio_mute_input': ('Audio mute?', 'Без звука?'),
-    'min_slice_length_checkbox': ('Min slice length', 'Минимальная длина фрагмента нарезки'),
-    'max_slice_length_checkbox': ('Max slice length', 'Максимальная длина фрагмента нарезки'),
+    'video_slice_checkbox': ('Slice video?', 'Нарезать видео?'),
+    'audio_mute_checkbox': ('Audio mute?', 'Без звука?'),
+    'shuffle_separately_checkbox': ('Shuffle separately?', 'Перемешать в общем?'),
+    'shuffle_in_general_checkbox': ('Shuffle in general?', 'Перемешать отдельно?'),
+    'min_slice_length_text': ('Min slice length', 'Минимальная длина фрагмента нарезки'),
+    'max_slice_length_text': ('Max slice length', 'Максимальная длина фрагмента нарезки'),
     'start_video_text': ('Start video', 'Начало видео'),
     'end_video_text': ('End video', 'Конец видео'),
     'audio_path_text': ('Audio path', 'Путь к аудио'),
     'audio_path_button': ('Search', 'Найти'),
     'start_audio_text': ('Start audio', 'Начало аудио'),
     'end_audio_text': ('End audio', 'Конец аудио'),
-    'audio_fade_in_checkbox': ('Fade in', 'Появление'),
-    'audio_fade_out_checkbox': ('Fade out', 'Затухание'),
+    'audio_fade_in_text': ('Fade in', 'Появление'),
+    'audio_fade_out_text': ('Fade out', 'Затухание'),
     'result_video_path_text': ('Result video path', 'Путь к экспорту готового видео'),
     'result_video_path_button': ('Search', 'Найти'),
     'render': ('Render', 'Рендер')
@@ -66,18 +83,23 @@ layout = [
     [gui.Text('Video path', key='video_path_text', enable_events=True), 
     gui.Input('', key='video_path_input', enable_events=True),
     gui.Button('Search', key='video_path_button', enable_events=True)],
-    [gui.Checkbox('Slice video?', key='video_slice_input', enable_events=True),
-    gui.Checkbox('Audio mute?', key='audio_mute_input', enable_events=True)],
+    [gui.Checkbox('Slice video?', key='video_slice_checkbox', enable_events=True),
+    gui.Checkbox('Audio mute?', key='audio_mute_checkbox', enable_events=True),
+    gui.Checkbox('Shuffle separately?', key='shuffle_separately_checkbox', enable_events=True),
+    gui.Checkbox('Shuffle in general?', key='shuffle_in_general_checkbox', enable_events=True)],
 
-    [gui.Checkbox('Min slice length', key='min_slice_length_checkbox', enable_events=True), 
+    [gui.Text('Min slice length', key='min_slice_length_text', enable_events=True), 
     gui.InputText('3', size = (5, 1), key='min_slice_length_input', enable_events=True)],
-    [gui.Checkbox('Max slice length', key='max_slice_length_checkbox', enable_events=True), 
+    [gui.Text('Max slice length', key='max_slice_length_text', enable_events=True), 
     gui.InputText('20', size = (5, 1), key='max_slice_length_input', enable_events=True)],
 
     [gui.Text('Start video', key='start_video_text'), 
     gui.InputText('', size = (5, 1), key='start_video_input', enable_events=True),
     gui.Text('End video', key='end_video_text'), 
     gui.InputText('', size = (5, 1), key='end_video_input', enable_events=True)],
+
+    [gui.Text('Start video', key='video_solution_text'),
+    gui.Combo(video_resolution_solutions_eng, default_value=video_resolution_solutions_eng[0], key='video_solution_select', enable_events=True)],
 
 	# Аудио параметры
     [gui.Text('Audio path', key='audio_path_text', enable_events=True), 
@@ -88,9 +110,9 @@ layout = [
     gui.Text('End audio', key='end_audio_text'), 
     gui.InputText('', size = (5, 1), key='end_audio_input', enable_events=True)],
 
-    [gui.Checkbox('Fade in', key='audio_fade_in_checkbox', enable_events=True), 
+    [gui.Text('Fade in', key='audio_fade_in_text', enable_events=True), 
     gui.InputText('0', size = (5, 1), key='audio_fade_in_input', enable_events=True),
-    gui.Checkbox('Fade out', key='audio_fade_out_checkbox', enable_events=True), 
+    gui.Text('Fade out', key='audio_fade_out_text', enable_events=True), 
     gui.InputText('0', size = (5, 1), key='audio_fade_out_input', enable_events=True)],
 
     [gui.Text('Result video path', key='result_video_path_text', enable_events=True), 
@@ -108,23 +130,24 @@ layout = [
 # 1 - Данные о видео материале для нарезки (двойной список)
 #   0 - путь к видео
 #   1 - нарезать видео? (True/False)
-#   2 - перемешать (True/False)
-#     Закидывает его в список и все видео, которые в него вошли 
-#     перемешивает. Если видео нарезано, то значит нарезанные 
-#     куски заносяться как отдельные видео
-#   3 - убрать звук (True/False)
-#   4 - минимальная длина slice
-#   5 - максимальная длина slice
+#   2 - убрать звук? (True/False)
+#   3 - перемешать отдельно? (True/False)
+#   4 - перемешать в общем? (True/False)
+#     Если перемешивает в общем, то закидывает в глобальный
+#     список видео. Если видео перемешивается отдельно тоже,
+#     то каждый нарезанный сегмент закидывается как отдельное видео
+#   5 - минимальная длина slice
+#   6 - максимальная длина slice
 #     Если числа совпадают, то нарезка будет происходить равными
 #     кусками. Минимум не может быть больше максимума и оба
 #     должны быть не меньше 0.5 секунды и не больше длины видео.
 #     Дефолтные значения - 3 секунды и 20 секунд соответственно
-#   6 - начало видео
-#   7 - конец видео
+#   7 - начало видео
+#   8 - конец видео
 #     Расстояние между числами должно быть не меньше 1 секунды и
 #     начало не может быть больше конца. Автоматически стоят на
 #     0 секунде и длине видео соответственно
-#   8 - вставить/заполнить/растянуть видео (3 варианта)
+#   9 - вставить/заполнить/растянуть видео (3 варианта)
 # 2 - Данные об аудио
 #   0 - путь, где храниться аудиофайл
 #   1 - начало аудио
